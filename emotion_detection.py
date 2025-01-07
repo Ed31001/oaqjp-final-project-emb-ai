1fa38c9d1c08d87b9d8ff9a9f678f5d85f256d31
Ed31001 <edouardkreidy@gmail.com>
2025-01-07 12:08:35 -0500

    ..

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,31 @@
 import requests
+import json
+
+def get_keys_by_value(d, value):
+    return [key for key, val in d.items() if val == value]
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    dominant_emotion_score = max(anger_score, disgust_score, fear_score, joy_score, sadness_score)
+
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': get_keys_by_value(formatted_response['emotionPredictions'][0]['emotion'], dominant_emotion_score)
+        }
+    
